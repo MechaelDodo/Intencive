a5bdd5f97d1d47e972368159a1a9483284ec2e9e
MechaelDodo <modod@bk.ru>
2025-01-28 22:16:32 +0300

    Initial commit with task1 and task2

---
+++ src/main/java/ru/aston/odod_mu/task1/Task1.java
@@ -0,0 +1,7 @@
+package main.java.ru.aston.odod_mu.task1;
+
+public class Task1 {
+    public static void main(String[] args){
+        System.out.println("task1");
+    }
+}
---
+++ src/main/java/ru/aston/odod_mu/task2/Task2.java
@@ -0,0 +1,7 @@
+package main.java.ru.aston.odod_mu.task2;
+
+public class Task2 {
+    public static void main(String[] args){
+        System.out.println("task2");
+    }
+}
