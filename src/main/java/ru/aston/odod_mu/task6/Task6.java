b85a8f98f64751f6877b657ae42cf3b8526c32b9
MechaelDodo <modod@bk.ru>
2025-01-30 00:21:26 +0300

    task 6 with Tree

--- src/main/java/ru/aston/odod_mu/task6/Task6.java
+++ src/main/java/ru/aston/odod_mu/task6/Task6.java
@@ -1,7 +1,38 @@
 package main.java.ru.aston.odod_mu.task6;
 
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
 public class Task6 {
     public static void main(String[] args){
-        System.out.println("hello");
+        Tree tree1 = new Tree(50, "tree1");
+        Tree tree2 = new Tree(100, "tree2", true);
+        Tree tree3 = new Tree();
     }
+}
+
+class Tree{
+    private int age;
+    private boolean alive;
+    private String name;
+
+    public Tree(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+
+    public Tree(int age, String name, boolean alive){
+        this.age = age;
+        this.name = name;
+    }
+
+    public Tree(){
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8)));
+        writer.println("Пустой конструктор без параметров сработал");
+        writer.flush();
+    }
+
 }
